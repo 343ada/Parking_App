b3fef96519dde9da4cd411cafa3240d7a46fb6ff
osamu <osamu.shibuya@ndsu.edu>
2024-05-05 21:36:30 -0500

    Implementing login and logout. Change the user database to built-in users.

--- parking_web_app/booking_app/urls.py
+++ parking_web_app/booking_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from booking_app import views
 
 urlpatterns=[
-    path('',views.index, name='index'),
+    path('booking',views.index, name='booking'),
     path('parking_availability/', views.parking_availability, name='parking_availability'),
     path('reserve_parking/', views.reserve_parking, name='reserve_parking'),
 ]
--- parking_web_app/registration_app/models.py
+++ parking_web_app/registration_app/models.py
@@ -22,6 +22,7 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+
 class Credit_Card(models.Model):
     card_fname = models.CharField(max_length=15)
     card_lname = models.CharField(max_length=20)
